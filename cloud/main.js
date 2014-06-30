1a1391587fefd515a740560fcf728b4f084c7929
曾劲 <king.fudan@gmail.com>
2014-06-30 19:49:43 +0800

    add email param

--- cloud/main.js
+++ cloud/main.js
@@ -11,24 +11,29 @@ AV.Cloud.define("checkIn", function(request, response) {
     // request.params.ssid
     // request.params.mac
     // request.user: 见https://cn.avoscloud.com/docs/cloud_code_guide.html#%E8%B0%83%E7%94%A8%E4%B8%80%E4%B8%AA%E5%87%BD%E6%95%B0
-    var location = new AV.GeoPoint({latitude: parseFloat(request.params.lat), longitude: parseFloat(request.params.lng)});
-    if (0.5 > location.kilometersTo(new AV.GeoPoint({latitude: 40.0, longitude: -30.0}))) {
-        var CheckIn = AV.Object.extend("CheckIn");
-        var checkIn = new CheckIn();
-        checkIn.save({
-            s: request.user,
-            location: location,
-            ssid: request.params.ssid,
-            mac: request.params.mac
-        }, {
-            success: function(checkIn) {
-                response.success(checkIn);
-            },
-            error: function(checkIn, error) {
-                response.error(error);
-            }
-        });
+    if (null != request.user) {
+        var location = new AV.GeoPoint({latitude: parseFloat(request.params.lat), longitude: parseFloat(request.params.lng)});
+        if (0.5 > location.kilometersTo(new AV.GeoPoint({latitude: 31.204736, longitude: 121.441307}))) {
+            var CheckIn = AV.Object.extend("CheckIn");
+            var checkIn = new CheckIn();
+            checkIn.save({
+                employee: request.user,
+                location: location,
+                ssid: request.params.ssid,
+                mac: request.params.mac,
+                email: request.params.email
+            }, {
+                success: function(checkIn) {
+                    response.success(checkIn);
+                },
+                error: function(checkIn, error) {
+                    response.error(error);
+                }
+            });
+        } else {
+            response.error("我读得书少，你不要骗我！");
+        }
     } else {
-        response.error("你确定你在公司？");
+        response.error("尚未登录无法签到");
     }
 });
